b8bf87d62c510f1308a78b4f773ac8d6ea57e8fd
Justin Li <justinnhli@gmail.com>
2020-01-27 10:33:16 -0800

    use venv for pegparse dependency

--- blib.py
+++ blib.py
@@ -10,7 +10,28 @@ from os.path import getmtime
 from pathlib import Path
 from subprocess import run
 
-from pegparse import ASTWalker, create_parser_from_file
+try:
+    from pegparse import ASTWalker, create_parser_from_file
+except ModuleNotFoundError as err:
+
+    def run_with_venv(venv):
+        """Run this script in a virtual environment.
+
+        Parameters:
+            venv (str): The virtual environment to use.
+        """
+        # pylint: disable = ungrouped-imports, reimported, redefined-outer-name, import-outside-toplevel
+        import sys
+        from os import environ, execv
+        from pathlib import Path
+        venv_python = Path(environ['PYTHON_VENV_HOME'], venv, 'bin', 'python3').expanduser()
+        if not venv_python.exists():
+            raise FileNotFoundError(f'could not find venv "{venv}" at executable {venv_python}')
+        if sys.executable == str(venv_python):
+            raise ImportError(f'no module {err.name} in venv "{venv}" ({venv_python})')
+        execv(str(venv_python), [str(venv_python), *sys.argv])
+
+    run_with_venv('blib')
 
 CACHE_DIR = Path('~/.cache/blib').expanduser().resolve()
 LIBRARY_DIR = Path('~/papers').expanduser().resolve()
